bf9eb8c2d74cb6484c89a34d293b6efe39e95ec0
Aaron Deas <deas.aaron@gmail.com>
2021-07-12 17:00:30 -0400

    Fixed issue with icons loading

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/styles.css",
-  "/icon-144x144.png",
+  "/icons/icon-144x144.png",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
