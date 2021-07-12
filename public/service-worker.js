5ddf4d067514e01ab266f32ebc4f3fdb3ee141b4
Aaron Deas <deas.aaron@gmail.com>
2021-07-11 22:17:55 -0400

    Added changes to service worker

---
+++ public/service-worker.js
@@ -0,0 +1,41 @@
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/favicon.ico',
+  '/styles.css',
+  '/icons/icon-144x144.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+
+    //activate
+  self.addEventListener('activate', (evt) => {
+      evt.waitUntil(
+          caches.keys().then(keyList => {
+              return Promise.all(
+                  keyList.map(key => {
+                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
+                          console.log('Removing all old cache data', key);
+                          return caches.delete(key);
+                      }
+                  })
+              )
+          })
+      );
+      self.clients.claim();
+  })
