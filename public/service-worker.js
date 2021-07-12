f870936f94e21f17baa1619126a0d86d44d8641b
Aaron Deas <deas.aaron@gmail.com>
2021-07-11 23:05:05 -0400

    Finished service-worker logic and created webmanifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,41 +1,78 @@
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/index.js',
-  '/favicon.ico',
-  '/styles.css',
-  '/icons/icon-144x144.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  "/",
+  "/index.html",
+  "/index.js",
+  "/favicon.ico",
+  "/styles.css",
+  "/icons/icon-144x144.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = 'static-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-    //activate
-  self.addEventListener('activate', (evt) => {
-      evt.waitUntil(
-          caches.keys().then(keyList => {
-              return Promise.all(
-                  keyList.map(key => {
-                      if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
-                          console.log('Removing all old cache data', key);
-                          return caches.delete(key);
-                      }
-                  })
-              )
-          })
+  self.skipWaiting();
+});
+
+//activate
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing all old cache data", key);
+            return caches.delete(key);
+          }
+        })
       );
-      self.clients.claim();
-  })
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.url.includes("/api")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
